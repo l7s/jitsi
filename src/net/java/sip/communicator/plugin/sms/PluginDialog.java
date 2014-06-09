98b8f72b9447c534e40a68face5c2d56148f12a1
veteran29 <veteran29q+github@gmail.com>
2014-06-09 15:18:44 +0200

    SMS Plugin: Change SpringLayout to GridBagLayout

--- src/net/java/sip/communicator/plugin/sms/PluginDialog.java
+++ src/net/java/sip/communicator/plugin/sms/PluginDialog.java
@@ -96,51 +96,74 @@ public class PluginDialog
     }
 
     private void setStyles()
-    {
-        SpringLayout layout = new SpringLayout();
-        /* From Label */
-        layout.putConstraint(SpringLayout.NORTH, fromLabel, 0, SpringLayout.NORTH, fromField);
-        layout.putConstraint(SpringLayout.WEST, fromLabel, 5, SpringLayout.WEST, mainPanel);
-        /* From Field */
-        layout.putConstraint(SpringLayout.NORTH, fromField, 5, SpringLayout.NORTH, mainPanel);
-        layout.putConstraint(SpringLayout.WEST, fromField, 5, SpringLayout.EAST, fromLabel);
-        layout.putConstraint(SpringLayout.EAST, fromField, 5, SpringLayout.EAST, mainPanel);
-        /* To Label */
-        layout.putConstraint(SpringLayout.NORTH, toLabel, 0, SpringLayout.NORTH, toField);
-        layout.putConstraint(SpringLayout.WEST, toLabel, 5, SpringLayout.WEST, mainPanel);
-        /* To Field */
-        layout.putConstraint(SpringLayout.NORTH, toField, 5, SpringLayout.SOUTH, fromField);
-        layout.putConstraint(SpringLayout.WEST, toField, 0, SpringLayout.WEST, fromField);
-        layout.putConstraint(SpringLayout.EAST, toField, 5, SpringLayout.EAST, mainPanel);
-        /* Text Field */
-        layout.putConstraint(SpringLayout.NORTH, textField, 5, SpringLayout.SOUTH, toField);
-        layout.putConstraint(SpringLayout.SOUTH, textField, -5, SpringLayout.NORTH, sendButton);
-        layout.putConstraint(SpringLayout.WEST, textField, 5, SpringLayout.WEST, mainPanel);
-        layout.putConstraint(SpringLayout.EAST, textField, 5, SpringLayout.EAST, mainPanel);
-        /* Send Button */
-        layout.putConstraint(SpringLayout.EAST, sendButton, 0, SpringLayout.EAST, textField);
-        layout.putConstraint(SpringLayout.SOUTH, sendButton, 5, SpringLayout.SOUTH, mainPanel);
-        /* Character label */
-        layout.putConstraint(SpringLayout.WEST, charactersLabel, 5, SpringLayout.WEST, textField);
-        layout.putConstraint(SpringLayout.NORTH, charactersLabel, 5, SpringLayout.SOUTH, textField);
-        
+    {       
+        GridBagLayout layout = new GridBagLayout();
+        this.mainPanel.setLayout(layout);
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.insets = new Insets(0, 5, 5, 0);
+        //row 0
+        c.weightx = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        this.mainPanel.add(fromLabel, c);
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridwidth = 2;
+        c.gridy = 0;
+        this.mainPanel.add(fromField, c);
+        //row 1
+        c.weightx = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.gridy = 1;
+        this.mainPanel.add(toLabel, c);   
+        c.weightx = 0.5;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 1;
+        c.gridwidth = 2;
+        c.gridy = 1;
+        this.mainPanel.add(toField, c);
+        //row 2
+        c.weightx = 1;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridwidth = 3;
+        c.gridy = 2;
+        this.mainPanel.add(textField, c);   
+        //row 3
+        c.weightx = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.gridy = 3;
+        this.mainPanel.add(charactersLabel, c);
+        c.weightx = 0;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 2;
+        c.gridwidth = 1;
+        c.gridy = 3;
+        this.mainPanel.add(sendButton, c);
+        /**/
         this.mainPanel.setBorder(
-            BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            BorderFactory.createEmptyBorder(10, 5, 5, 10));
         
         this.fromField.setEditable(false);
         this.toLabel.setFont(toLabel.getFont().deriveFont(Font.BOLD));
         this.fromLabel.setFont(toLabel.getFont().deriveFont(Font.BOLD));
         
-        this.textField.setColumns(45);
+        this.textField.setColumns(20);
         this.textField.setLineWrap(true);
-        this.textField.setRows(5);
+        this.textField.setRows(9);
         this.textField.setWrapStyleWord(true);
         this.textField.setDocument(doc);
         this.textField.setBorder( toField.getBorder() );     
         
         this.mainPanel.setLayout(layout);
-        this.mainPanel.setPreferredSize(new Dimension(225, 210) );
-        layout.minimumLayoutSize(mainPanel);
     }
     
     private void setCharacterCount()
